ff51f188e20704d7bb296bfc81bac71deee0b7dd
Gavin Soko <sokolofg@gmail.com>
2018-05-09 18:59:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,6 +2,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray (arr, element) {
   return ([...arr]);
 }
+
 function destructivelyAddElementToBeginningOfArray (arr, element) {
-  return arr.unshift(element) {
+  return arr.unshift(element) 
 }
