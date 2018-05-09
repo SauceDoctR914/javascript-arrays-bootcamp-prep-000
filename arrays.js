638cd11318e3b935c9905a83f22488b94e235db8
Gavin Soko <sokolofg@gmail.com>
2018-05-09 20:33:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,7 +15,7 @@ function destructivelyRemoveElementFromBeginningOfArray (array) {
   return array;
 }
 function removeElementFromBeginningOfArray(array){
-return array.slice(0);
+return array.slice(1);
 }
 
 function destructivelyRemoveElementFromEndOfArray (array) {
